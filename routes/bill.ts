5796adaf1cb54a59ef406ee41766a94dbc93a462
mamdouh <mo.mamdouh.saad@gmail.com>
2020-02-18 00:48:24 +0200

    sr2

--- routes/bill.ts
+++ routes/bill.ts
@@ -59,8 +59,17 @@
                 const table_number = req.body.table_number;
                 const returnedBill = await Bill.findOne({ table_number:table_number,bill_status:"Stored" }) //TODO : optimization
                 if(returnedBill){
-                    const result = await Bill.updateOne({_id:returnedBill._id},req.body);
-                    console.log(result)
+                    console.log('returnedBill')
+
+                    console.log(returnedBill)
+                    console.log('req.body')
+
+                    console.log(req.body)
+                    const result = await Bill.updateOne({_id:returnedBill._id},req.body)
+                    console.log('result.lines')
+                        if(result){
+                            console.log(result)
+                        }
                     }else{
                     var addedbill = await new Bill(req.body);
                     await addedbill.save();
@@ -84,8 +93,9 @@
     })
     billRouter.get("/:table_no",async(req,res)=>{ // get one Bill by id
         try {
-            const returnedBill = await Bill.find({ table_number: req.params.table_no,bill_status:"Stored" }).populate('lines.product')
-                console.log(returnedBill)
+            const returnedBill = await Bill.findOne({ table_number: req.params.table_no,bill_status:"Stored" }).populate('lines.product')
+            console.log("table no")   
+            console.log(returnedBill)
             return res.status(200).json({response:returnedBill,success:true});
           } catch (err) {
             return res.status(400).json({response:err,success:false,responseMsg:"Error occured while retreive one Bill with id "});
